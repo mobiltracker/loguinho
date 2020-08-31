a97df987100c3de7d9f91932868d0cfce07ec888
mlcruz1 <mlcruz@inf.ufrgs.br>
2020-08-31 13:43:53 -0300

    watch done

--- src/watch.rs
+++ src/watch.rs
@@ -7,13 +7,14 @@ use crate::{
     Watch,
 };
 use std::error::Error;
-use std::time::Duration;
+use std::{collections::HashSet, time::Duration};
 
 pub async fn watch_main(w: Watch, client: CloudWatchLogsClient) -> Result<(), Box<dyn Error>> {
     let log_groups = get_all_log_groups(&client, &w.input.unwrap_or("".to_owned())).await?;
+    let mut log_events_filter: HashSet<Option<String>> = HashSet::new();
 
     loop {
-        let curr_timestamp = chrono::offset::Utc::now().timestamp_millis() - 10000;
+        let curr_timestamp = chrono::offset::Utc::now().timestamp_millis() - 30000;
 
         for log_group_chunk in log_groups.chunks(5) {
             for log_group in log_group_chunk {
@@ -25,9 +26,12 @@ pub async fn watch_main(w: Watch, client: CloudWatchLogsClient) -> Result<(), Bo
                         if !events.is_empty() {
                             let log_name = log_group.log_group_name.as_ref().map(|s| s.as_str());
 
-                            events
-                                .iter()
-                                .for_each(|event| pretty_print_log_event(event, log_name));
+                            events.iter().for_each(|event| {
+                                if !log_events_filter.contains(&event.event_id) {
+                                    pretty_print_log_event(event, log_name);
+                                    log_events_filter.insert(event.event_id.clone());
+                                }
+                            });
                         }
                     }
                     Err(err) => match err {
@@ -51,5 +55,11 @@ pub async fn watch_main(w: Watch, client: CloudWatchLogsClient) -> Result<(), Bo
         }
 
         sleep(Duration::from_secs(5)).await;
+
+        if log_events_filter.len() > 100000 {
+            println!("clearing log filter...");
+            log_events_filter = HashSet::new();
+            sleep(Duration::from_secs(20)).await;
+        }
     }
 }
